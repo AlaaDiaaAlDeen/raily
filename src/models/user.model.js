bc9d4db8fbbe3b6a0e4e3ee1122de0547e11088e
zainab8585 <zeinabeng900@gmail.com>
2021-04-11 04:59:30 +0200

    udate user model schema add cloums
    add user cloums

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,10 +2,37 @@ const mongoose = require('mongoose')
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const validator = require('validator')
-
-
+// create user schema
 const userSchema = new mongoose.Schema({
-    
+  name:{
+    type:String,
+    trim:true,
+    required:true
+},
+email:{
+  type:String,
+  trim:true,
+  required:true,
+  unique:true,
+  validate(value){
+      if(!validator.isEmail(value)) throw new Error('invalid email')
+  }
+},
+phone:{
+  type:String,
+  validate(value){
+      if(!validator.isMobilePhone(value, ['ar-EG'])) throw new Error('egyption mobile needed')
+  }
+},
+password:{
+  type:String,
+  trim:true,
+  required:true
+}, 
+role:{
+  type:String,
+  required:true
+},
 })
 
 
