2c42f70b0d14126e1e86d649d8810f68284f1747
= <AlaaDiaaAlDeen>
2021-04-16 17:15:08 +0200

    added roles and privileges models

--- api/src/app.js
+++ api/src/app.js
@@ -2,11 +2,13 @@ require('./db/db')
 const express = require ('express')
 const cors = require('cors')
 const userRoutes = require('./routes/user.routes.js')
-const restRoutes = require('./routes/rest.routes.js')
+const PrivilegesRoutes = require('./routes/privileges.routes.js')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(userRoutes)
+app.use(PrivilegesRoutes)
+
 
 module.exports = app
---
+++ api/src/routes/privileges.routes.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const RolesModel = require('../models/user-roles.model.js')
+const PrivilegesModel = require('../models/privileges.model.js')
+
+const router = new express.Router()
+
+
+
+module.exports = router
